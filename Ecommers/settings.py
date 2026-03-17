4b7069bec42856db547d6eb69b25f9dff278466a
ShrishMaruge <shrish.maruge99@gmail.com>
2026-03-17 21:59:11 +0530

    Update settings.py

--- Ecommers/settings.py
+++ Ecommers/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x+rm&^vfh!40pl_g&n7w+qvm*+p^$npnny+5-)k^na6e(b-wo3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
