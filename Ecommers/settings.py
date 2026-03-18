6d90aab37e01313085752c380b0a9dcab3f39889
ShrishMaruge <shrish.maruge99@gmail.com>
2026-03-18 21:59:19 +0530

    Update settings.py

--- Ecommers/settings.py
+++ Ecommers/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x+rm&^vfh!40pl_g&n7w+qvm*+p^$npnny+5-)k^na6e(b-wo3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['ecommers-luxury-django.onrender.com']
 
